f537d590a3a35d1b6fe801201e0b4d68dd67cf6e
htdguide <htdguide@DESKTOP-CK1R0NE>
2023-09-12 23:42:57 +1200

    UI ready

--- Sorting Algorithms/main.cpp
+++ Sorting Algorithms/main.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <array>
 #include <iostream>
+#include <string>
+
 using namespace std;
 const int dataSize = 78;
 int dataArray[dataSize];
@@ -12,14 +14,14 @@ void drawDataRectangle(int posX, int posY, int width, int height, Color color) /
 	DrawRectangleLines(posX, posY, width, height, GRAY);
 }
 
-void drawData(int data[]) //Drawing method for the arrays
+void drawData(int data[], Color color) //Drawing method for the arrays
 {
 	int x = 10; //gap between lines
 	int y = 590; 
 
 	for (int i = 0; i < dataSize; i++)
 	{
-		drawDataRectangle(x * i + 10, y - data[i], 10, data[i], WHITE);
+		drawDataRectangle(x * i + 10, y - data[i], 10, data[i], color);
 	}
 }
 
@@ -59,27 +61,62 @@ void bubbleSort(int data[], int i)
 
 int main() 
 {
+	int attempts = 0;
+	int tempDataSize = dataSize;
+	int FPS = 60;
 	int counter = 0;
-	InitWindow(800, 600, "Game");
-	SetWindowState(FLAG_VSYNC_HINT);
+	string s = "Attempts: ";
+	string msg, sort, bubble;
+	InitWindow(800, 600, "Sorting");
+	SetTargetFPS(120);
 	fillData(dataArray);
 
 	while (!WindowShouldClose())
 	{
+		SetTargetFPS(FPS);
 		BeginDrawing();
+		DrawFPS(700, 10);
 		ClearBackground(BLACK);
-		drawData(dataArray);
+		msg = s + to_string(attempts);
+		sort = bubble;
+		DrawText(msg.c_str(), 10, 10, 30, WHITE);
+		DrawText(sort.c_str(), 10, 40, 20, WHITE);
+		drawData(dataArray, WHITE);
 
 		if (IsKeyDown(KEY_B))
 		{
 			bubbleSort(dataArray, counter);
-			if (counter < dataSize)
+			bubble = "Bubble ";
+			if (counter < tempDataSize)
 			{
 				counter++;
+				attempts++;
 			}
-			else counter = 0;
+			else if (tempDataSize != 0)
+			{
+				counter = 0;
+				tempDataSize--;
+				attempts++;
+			}
+			else drawData(dataArray, GREEN);
 		}
 
+		if (IsKeyDown(KEY_UP) && FPS < 300)
+		{
+			FPS++;
+		}
+		if (IsKeyDown(KEY_DOWN) && FPS > 24)
+		{
+			FPS--;
+		}
+		if (IsKeyDown(KEY_R))
+		{
+			fillData(dataArray);
+			attempts = 0;
+			tempDataSize = dataSize;
+			counter = 0;
+			bubble = " ";
+		}
 
 
 
